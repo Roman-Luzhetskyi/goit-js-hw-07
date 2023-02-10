034db194ff341fc7bd65c2f0976beffd5fa540d3
Roman-Luzhetskyi <113299200+Roman-Luzhetskyi@users.noreply.github.com>
2023-02-10 19:54:48 +0100

    task-01

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const totalCategories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${totalCategories.length}`);
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.firstElementChild.textContent}
-    Elements: ${categories.lastElementChild.children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
